b67cc0917becd30c77e86352af12fb8bf8d05a3d
codefoxlk <eshannimeshadesigns@gmail.com>
2022-07-13 14:10:25 +0530

    init

---
+++ lib/bloc_basics/blocs/counter_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:bloc/bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    print('$error, $stackTrace');
+    super.onError(error, stackTrace);
+  }
+}
